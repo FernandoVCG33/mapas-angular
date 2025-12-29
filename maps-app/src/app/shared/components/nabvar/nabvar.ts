cb8cb7f6627688ba01562ef1c4c2f14da03f557d
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-29 11:33:36 -0400

    Se mejoro el nabvar y todp

--- maps-app/src/app/shared/components/nabvar/nabvar.ts
+++ maps-app/src/app/shared/components/nabvar/nabvar.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {routes} from '../../../app.routes';
+import {NavigationEnd, Router, RouterLink} from '@angular/router';
+import {filter, map, tap} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-nabvar',
-  imports: [],
+  imports: [
+    AsyncPipe,RouterLink
+  ],
   templateUrl: './nabvar.html',
 })
 export class Nabvar {
-    routes=routes.map(route =>({
-      path: route.path,
-      title:` ${route.title ?? 'Maps in Angular'}`,
-    }));
+  router = inject(Router)
+  routes = routes.map(route => ({
+    path: route.path,
+    title: ` ${route.title ?? 'Maps in Angular'}`,
+  })).filter( route => route.path !== '**');
+  pageTitle$ = this.router.events.pipe(
+    filter(event=> event instanceof NavigationEnd),
+    tap(event => console.log(event)),
+    map(event =>  event.url ),
+    map(url =>  routes.find(route => `/${route.path}` === url )?.title ?? 'Maps in Angular'),
+  );
 }
