57b2c2b75cd4ea7c6b5a0d8e3ebffc739232b2df
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-30 12:39:16 -0400

    Mas cambios y enrutamientos

--- maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, AfterViewInit, viewChild } from '@angular/core';
+import {Component, ElementRef, AfterViewInit, viewChild, signal} from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
 
@@ -13,13 +13,25 @@ mapboxgl.accessToken = environment.mapboxKey;
       width: 100vw;
       height: calc(100vh - 64px);
     }
+    #controls{
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px rgba(0,0,0,0.3);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }
   `
 })
 export class FullscreenMapPage implements AfterViewInit {
 
   // Corregido el nombre de la propiedad
   mapDiv = viewChild<ElementRef>('map');
-
+  zoom=signal(14);
   ngAfterViewInit(): void {
     if (!this.mapDiv()?.nativeElement) throw 'Elemento #map no encontrado';
 
