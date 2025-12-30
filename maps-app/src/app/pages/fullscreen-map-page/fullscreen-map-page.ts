ae8b6cb4a89d77685ac8cb14a07c0c99bc9491b2
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-30 15:51:39 -0400

    Add coordinates display and update zoom functionality in fullscreen map

--- maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, AfterViewInit, viewChild, signal, effect} from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
+import {DecimalPipe, JsonPipe} from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 
@@ -8,6 +9,9 @@ mapboxgl.accessToken = environment.mapboxKey;
   selector: 'app-fullscreen-map-page',
   standalone: true, // Añadido para que sea un componente standalone
   templateUrl: './fullscreen-map-page.html',
+  imports:[
+    DecimalPipe, JsonPipe
+  ],
   styles: `
     div {
       width: 100vw;
@@ -38,6 +42,12 @@ export class FullscreenMapPage implements AfterViewInit {
   })
 
   zoom=signal(14);
+
+  coordinates=signal({
+    lng:-74.5,
+    lat:40
+  });
+
   ngAfterViewInit(): void {
     if (!this.mapDiv()?.nativeElement) throw 'Elemento #map no encontrado';
 
@@ -45,15 +55,24 @@ export class FullscreenMapPage implements AfterViewInit {
 
     if (!mapElement) throw 'Elemento nativo #map no encontrado';
 
+    const {lat,lng}=this.coordinates();
     const map = new mapboxgl.Map({
       container: mapElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: [lat, lng], // starting position [lng, lat]
       zoom: this.zoom(), // starting zoom
     });
-    this.map.set(map);
+    this.mapListener(map);
   }
   mapListener(map:mapboxgl.Map){
-
+    map.on('zoomend', (event)=>{
+      const newZoom= event.target.getZoom();
+      this.zoom.set(newZoom);
+    });
+    map.on('moveend',(event)=>{
+      const center=map.getCenter();
+      this.coordinates.set(center);
+    });
+    this.map.set(map);
   }
 }
